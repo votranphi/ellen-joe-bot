3b1f3b090daad94e43f8fa193e40b6ab8839e834
Vo Tran Phi <songokupkj@gmail.com>
2025-12-25 15:05:54 +0700

    fix(main): error in callling api

--- src/cogs/chat.py
+++ src/cogs/chat.py
@@ -33,7 +33,7 @@ class Chat(commands.Cog):
         else:
             genai.configure(api_key=api_key)
             self.model = genai.GenerativeModel(
-                model_name="gemini-1.5-flash",
+                model_name="gemini-2.5-flash",
                 system_instruction=SYSTEM_INSTRUCTION
             )
 
